55d9fd00b250b1087df5cb9c10e479b19a2054ea
Ian Karanja <optimusglitch@gmail.com>
2024-06-26 13:59:01 +0300

    updates

--- bot/bot.sh
+++ bot/bot.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-# System Request : Debian 9+/Ubuntu 18.04+/20+
-# Develovers » Darkanonc
-# Email      » darkanoncc@gmail.com
-# telegram   » https://t.me/darkanonc
-# whatsapp   » wa.me/+254706376682
+# System Request: Debian 9+/Ubuntu 18.04+/20+
+# Developer: Darkanonc
+# Email: darkanoncc@gmail.com
+# Telegram: https://t.me/darkanonc
+# WhatsApp: wa.me/+254706376682
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 # Darkanonc
 
-NS=$( cat /etc/xray/dns )
-PUB=$( cat /etc/slowdns/server.pub )
+NS=$(cat /etc/xray/dns)
+PUB=$(cat /etc/slowdns/server.pub)
 domain=$(cat /etc/xray/domain)
-#color
+# color
 grenbo="\e[92;1m"
 NC='\e[0m'
-#install
+# install
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
@@ -31,15 +31,15 @@ pip3 install -r kyt/requirements.txt
 clear
 echo ""
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[1;97;101m           » TAMBAH BOT PANEL «           \e[0m"
+echo -e "\e[1;97;101m           » ADD BOT PANEL «           \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
-echo -e "${grenbo}[»] Creat Bot and Token Bot : @BotFather${NC}"
-echo -e "${grenbo}[»] Info Id Telegram : @MissRose_bot${NC}"
+echo -e "${grenbo}Tutorial Create Bot and Telegram ID${NC}"
+echo -e "${grenbo}[»] Create Bot and Bot Token: @BotFather${NC}"
+echo -e "${grenbo}[»] Telegram ID Info: @MissRose_bot${NC}"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-read -e -p "[»] Input your Bot Token   : " bottoken
-read -e -p "[»] Input Your Id Telegram : " admin
+read -e -p "[»] Input your Bot Token: " bottoken
+read -e -p "[»] Input Your Telegram ID: " admin
 echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
 echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
 echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
@@ -61,24 +61,24 @@ Restart=always
 WantedBy=multi-user.target
 END
 
-systemctl start kyt 
+systemctl start kyt
 systemctl enable kyt
 systemctl restart kyt
 cd /root
 rm -rf kyt.sh
-echo "Input Data Berhasil Diproses!"
-echo "Your Data Bot Telegram"
+echo "Data Input Processed Successfully!"
+echo "Your Telegram Bot Data"
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo "Token Bot      : $bottoken"
-echo "Admin          : $admin"
-echo "Domain         : $domain"
-# echo "Pub            : $PUB"
-# echo "Host           : $NS"
+echo "Bot Token: $bottoken"
+echo "Admin: $admin"
+echo "Domain: $domain"
+# echo "Pub: $PUB"
+# echo "Host: $NS"
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-echo "Setting Bot Success!"
+echo "Bot Setting Success!"
 sleep 2
 clear
 
-echo "Installations complete, type /menu on your bot"
-read -p "Press Enter Back To menu"
+echo "Installation complete, type /menu on your bot"
+read -p "Press Enter to go back to the menu"
 menu
